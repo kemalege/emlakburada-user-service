007e97d04613deec9d48b456bc146857b112c218
kemalege <cirpanliege@gmail.com>
2024-07-22 03:59:59 +0300

    Create UserGetByEmailRequest

---
+++ src/main/java/com/patika/emlakburadauserservice/dto/request/UserGetByEmailRequest.java
@@ -0,0 +1,15 @@
+package com.patika.emlakburadauserservice.dto.request;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserGetByEmailRequest {
+
+    private String email;
+}
