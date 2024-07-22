26ec64a7970fcbecd221a5ca9288c25d5aa787d5
kemalege <cirpanliege@gmail.com>
2024-07-22 04:00:40 +0300

    Update exception messages

--- src/main/java/com/patika/emlakburadauserservice/exception/ExceptionMessages.java
+++ src/main/java/com/patika/emlakburadauserservice/exception/ExceptionMessages.java
@@ -6,7 +6,8 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor(access = AccessLevel.PRIVATE)
 public class ExceptionMessages {
 
-    public static final String USER_NOT_FOUND = "user bulunamadı.";
+    public static final String USER_NOT_FOUND = "kullanıcı bulunamadı.";
+    public static final String USER_NOT_FOUND_BY_EMAIL = "geçerli email ile kayıtlı kullanıcı bulunamadı.";
     public static final String USER_NOT_ACTIVE = "user aktif değil.";
     public static final String EMAIL_ALREADY_EXIST = "bu email ile kayıtlı kullanıcı var";
 }
